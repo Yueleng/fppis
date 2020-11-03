4bb6a4f183e4aa4fe106cb9c0f9e009734bd1bbd
yueleng <wangyueleng@gmail.com>
2020-11-03 02:00:48 -0500

    lecture notes week4

--- week3/classHierarchies/src/main/scala/example/intsets.scala
+++ week3/classHierarchies/src/main/scala/example/intsets.scala
@@ -1,5 +1,7 @@
 package example
 
+import scala.annotation.tailrec
+
 object intsets extends App {
   println("Welcome to the Scala worksheet")
   val t1 = new NonEmpty(3, Empty, Empty)
@@ -7,19 +9,32 @@ object intsets extends App {
   println(t1)
   println(t2)
 
+  @tailrec
   def nth[T](n: Int, xs: List[T]): T = {
     if (xs.isEmpty) throw new IndexOutOfBoundsException
     else if (n == 0) xs.head
     else nth(n-1, xs.tail)
   }
 
-  val list = new Cons(1, new Cons(2, new Cons(3, new Nil)))
+  val list = new Cons(1, new Cons(2, new Cons(3, Nil)))
 
 
   println(nth(2, list))
-  println(nth(3, list))
-  println(nth(-1, list))
+  // println(nth(3, list))
+  // println(nth(-1, list))
 
+  // week 4
+  val x: List[String] = Nil
+
+
+  def show(e: Expr): String = e match {
+    case Number(x) => x.toString
+    case Sum(l, r) => show(l) + " + " + show(r)
+    case Var(x) => x
+    case Prod(x, y) => "(" + show(x) + ") * (" + show(y) + ")"
+  }
+
+  println(show(Sum(Number(1), Number(44))))
 
 }
 
@@ -57,10 +72,11 @@ class NonEmpty(elem: Int, left: IntSet, right: IntSet) extends IntSet {
 
 }
 
-trait List[T] {
+trait List[+T] {
   def isEmpty: Boolean
   def head: T
   def tail: List[T]
+  def prepend[U >: T](elem: U): List[U] = new Cons(elem, this)
 }
 
 // head and tail are already evaluated upon initialization.
@@ -68,8 +84,29 @@ class Cons[T] (val head: T, val tail: List[T]) extends List[T] {
   def isEmpty = false
 }
 
-class Nil[T] extends List[T] {
+//class Nil[T] extends List[T] {
+//  def isEmpty = true
+//  def head = throw new NoSuchElementException("Nil.head")
+//  def tail = throw new NoSuchElementException("Nil.tail")
+//}
+
+object Nil extends List[Nothing] {
   def isEmpty = true
   def head = throw new NoSuchElementException("Nil.head")
   def tail = throw new NoSuchElementException("Nil.tail")
 }
+
+
+// Week 4
+object List {
+  // List(1, 2) = List.apply(1, 2)
+  def apply[T](x1: T, x2: T): List[T] = new Cons(x1, new Cons(x2, Nil))
+  def apply[T](x1: T): List[T] = new Cons(x1, Nil)
+  def apply[T](): List[Nothing] = Nil
+}
+
+trait Expr
+case class Number(n: Int) extends Expr
+case class Sum(e1: Expr, e2: Expr) extends Expr
+case class Var(s: String) extends Expr
+case class Prod(e1: Expr, e2: Expr) extends Expr
